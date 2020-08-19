52d0be1b7982599fcd6f652a704c0dceb0404247
DTomusk <david.tomusk@gmail.com>
2020-08-19 16:21:56 +0100

    Refactoring

--- main.py
+++ main.py
@@ -10,43 +10,42 @@ canvas.pack()
 ORIGIN = [canvas_width/2, canvas_height/2]
 GRID_RADIUS = 500
 
-def drawArc(angle):
-	# pretend we have a unit circle and then convert to coordinates 
+def calc(angle):
 	x = math.cos(angle)
-	print("x: " + str(x))
 	y = math.sin(angle)
-	print("y: " + str(y))
 	gradient = y/x 
-	print("Gradient: " + str(gradient))
 	normal = -1/gradient
-	print("Normal: " + str(normal))
 	focus = x - (y/normal)
-	print("Focus: " + str(focus))
+	return (x, y, focus)
 
-	# now convert to coordinates 
-	# everything is scaled up by a factor of grid_radius 
+def convertToScreen(x, y, focus):
 	c_focus = (focus * GRID_RADIUS) + ORIGIN[0]
 	c_y = int((y * GRID_RADIUS) + ORIGIN[1])
 	c_x = int((x * GRID_RADIUS) + ORIGIN[0])
-
-	print("Corrected y: " + str(c_y))
-	print("Corrected focus: " + str(c_focus))
-	print("Screen y: " + str(ORIGIN[1]))
-	print("Opposite y: " + str((2*ORIGIN[1])-c_y))
 	arc_radius = math.sqrt((c_y-ORIGIN[1])**2 + (c_x-c_focus)**2)
+	return (c_x, c_y, c_focus, arc_radius)
+
+def drawArc(angle):
+
+	(x, y, focus) = calc(angle)
+
+	(c_x, c_y, c_focus, arc_radius) = convertToScreen(x, y, focus)
 
 	for j in range(2*int(ORIGIN[1])-c_y,c_y):
 		i = math.sqrt(arc_radius**2-(j-ORIGIN[1])**2)+c_focus
-		#print(str(i) + ", " + str(j))
 		canvas.create_rectangle(i-1,j-1,i,j)
 
-def main():
+def drawInitial():
 	x = ORIGIN[0]
 	y = ORIGIN[1]
 	canvas.create_oval(x-GRID_RADIUS,y-GRID_RADIUS,x+GRID_RADIUS,y+GRID_RADIUS)
 	canvas.create_rectangle(x-GRID_RADIUS,y-GRID_RADIUS,x+GRID_RADIUS,y+GRID_RADIUS)
 	canvas.create_line(x,y-GRID_RADIUS,x,y+GRID_RADIUS)
 	canvas.create_line(x-GRID_RADIUS,y,x+GRID_RADIUS,y)
+
+def main():
+	drawInitial()
+	
 	for x in range(17,32):
 		if x % 16 != 0:
 			drawArc(x*math.pi/32)
